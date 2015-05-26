01df1151ad751d8d0c7b9ccce0de6c61c125270b
Prabhu Ram Kumar <Prabhu.rk90@gmail.com>
2015-05-26 18:37:52 +0530

    Added SIGN UP client side validation [ bug in mobile field ]

--- Javascript/Validations.js
+++ Javascript/Validations.js
@@ -1,13 +1,62 @@
 function validation() {
-	var fname=document.getElementById("firstname").value;
-	if (fname == null || fname == "") {
-        alert(" First Name must be filled out");
 
-		$('#firstname').addClass('alert');
-		 return false;
+	var fname=document.getElementById("signup_fname");
+	var lname=document.getElementById("signup_lname");
+	var email=document.getElementById("signup_email");
+	var pass=document.getElementById("signup_pass");
+	var cpass=document.getElementById("signup_cpass");
+	var mob=document.getElementById("signup_mobile");
 
-    }
+	empty(fname);
+	empty(lname);
+	empty(email);
+	empty(pass);
+	empty(cpass);
+	passcheck(pass,cpass);
+	empty(mob);
+
+
 }
 
+function passcheck(field1,field2){
 
+	/* PASSWORD MATCH validation */
+
+	if((field2!=null)&&(field1.value!=field2.value))
+	{
+		field2.setCustomValidity("Password doesn't match");
+	}
+	else
+	{
+		field2.setCustomValidity('');
+	}
+}
+
+function empty(field1)
+{
+
+	/* EMPTY field validation */
+
+	if(field1.value == '')
+	{
+		field1.setCustomValidity(field1.getAttribute("name")+" is required");
+	}
+
+	/* MOBILE number only check */
+
+	else if(field1.getAttribute("name")=="mobile")
+	{
+		if(isNaN(field1.value))
+		{
+			field1.setCustomValidity("Enter only numbers");
+		}
+	}
+
+	/* no errors setting */
+
+	else
+	{
+		field1.setCustomValidity('');
+	}
+}
 
